165fcc1faff090ca165f77e793bc8c59e50007da
jaishankar7655 <Jaishankar7655@gmail.com>
2025-07-23 21:32:11 +0530

    Update pushToDockerHub.groovy

--- vars/pushToDockerHub.groovy
+++ vars/pushToDockerHub.groovy
@@ -1,1 +1,22 @@
+def call(String imageName, String tag = "latest") {
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerHubCred',
+        usernameVariable: 'DOCKER_HUB_USER',
+        passwordVariable: 'DOCKER_HUB_PASS'
+    )]) {
+        echo 'Logging into Docker Hub...'
+        sh """
+            docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASS"
+        """
 
+        echo "Tagging the image as \$DOCKER_HUB_USER/${imageName}:${tag}"
+        sh """
+            docker tag django-app-django:latest \$DOCKER_HUB_USER/${imageName}:${tag}
+        """
+
+        echo "Pushing the image to Docker Hub..."
+        sh """
+            docker push \$DOCKER_HUB_USER/${imageName}:${tag}
+        """
+    }
+}
